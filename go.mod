6d6282c082161d9c86bc373b7c6d3dd505480959
Saiki <e@eeuu.cc>
2024-08-09 23:50:56 +0800

    fix

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/DaRealFreak/cloudflare-bp-go
 go 1.17
 
 require (
-	github.com/EDDYCJY/fake-useragent v0.2.0
 	github.com/PuerkitoBio/goquery v1.7.1 // indirect
 	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/stretchr/testify v1.4.0
--- round_tripper.go
+++ round_tripper.go
@@ -5,7 +5,6 @@ package cloudflarebp
 
 import (
 	"crypto/tls"
-	browser "github.com/EDDYCJY/fake-useragent"
 	"net/http"
 )
 
@@ -76,7 +75,7 @@ func GetDefaultOptions() Options {
 		Headers: map[string]string{
 			"Accept":          "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
 			"Accept-Language": "en-US,en;q=0.5",
-			"User-Agent":      browser.Firefox(),
+			"User-Agent":      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0",
 		},
 	}
 }
